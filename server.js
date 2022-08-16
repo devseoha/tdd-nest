ce850414866d4153f38a0bc3e8f520ef7006d265
“summer” <devsh4030@gmail.com>
2022-08-16 09:57:55 +0900

    Add. 몽고디비 연결

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const productRoutes = require('./routes');
-
+const mongoose = require('mongoose');
 const PORT = 5000;
 
 const app = express();
+console.log(process.env.PASSWORD);
+
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@tddtest.0lekl7f.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`)
+    .then(() => console.log('MongoDb Connected...'))
+    .catch(err => console.log(err));
+
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
