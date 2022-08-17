91705fddd67107399dc6740a0196a8898e5d4d1e
“summer” <devsh4030@gmail.com>
2022-08-17 21:02:33 +0900

    Add. jest 테스트 코드 생성

---
+++ test/unit/products.test.js
@@ -0,0 +1,9 @@
+describe("Calculate", () => {
+  test("two plug two is four", () => {
+    expect(2 + 2).not.toBe(5);
+  });
+
+  test("two plug two is not five", () => {
+    expect(2 + 2).not.toBe(5);
+  });
+});
