65541bde9b63687e59ed3fdcf5be68be95fd13a3
summer <devsh4030@gmail.com>
2022-08-28 12:50:35 +0900

    Add. 🐱status, 반환데이터 확인하는 케스트 케이스 작성

--- controller/products.js
+++ controller/products.js
@@ -1,5 +1,13 @@
 const productModel = require("../models/Product");
 
-exports.createProduct = (req, res, next) => {
+exports.createProduct = async (req, res, next) => {
   productModel.create(req.body);
+  /* test => expect(res.statusCode).toBe(201);
+   * code => res.status(201)
+   * 반환하는 코드가 동일한지 확인
+   * test => expect(res._isEndCalled()).toBeTruthy();
+   * code => res.send();
+   * 트루로 반환하는지 확인
+   * */
+  res.status(201).send(false);
 };
